4d2ceaac957e15286a4ba29ff64fadea480364f3
Erismirnixvega <9023832@student.zadkine.nl>
2025-11-24 12:49:04 +0100

    Maken van Project (WK1)

---
+++ P2-Project/Week1-P2/README.md
@@ -0,0 +1,14 @@
+# Over Project (Week 1)
+
+## Inhoud
+*Dit is een website dat wordt geleken op een moderne bankapplicatie.*
+
+
+## Reden
+*Handig voor mijn ontwikkeling voor vaardigheden en laten zien wat ik kan doen.*
+
+
+### Technologieën 
+- HTML
+- CSS
+- JavaScript
---
+++ P2-Project/Week1-P2/script.js
@@ -0,0 +1,13 @@
+function ReDirect() {
+    if (confirm("Bevestig dat u wilt aan uw rekeningen beheren?")) {
+        window.location.href = "rekeningen.html";
+    }
+}
+
+
+function ReDirect2() {
+    if (confirm("Bevestig dat u wilt naar een geld overmaken?")) {
+        window.location.href = "overschrijven.html";
+
+    }
+}
